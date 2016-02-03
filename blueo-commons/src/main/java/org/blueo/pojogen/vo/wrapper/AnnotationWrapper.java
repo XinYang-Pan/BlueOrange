5e26663098603e50c3015c1fa969745bfa4429a3
SeanPAN <seanpan2008@gmail.com>
2016-02-03 23:52:58 +0800

    Pojo gen re-org

--- blueo-commons/src/main/java/org/blueo/pojogen/vo/wrapper/AnnotationWrapper.java
+++ blueo-commons/src/main/java/org/blueo/pojogen/vo/wrapper/AnnotationWrapper.java
@@ -2,24 +2,37 @@ package org.blueo.pojogen.vo.wrapper;
 
 import java.lang.annotation.Annotation;
 
+import com.google.common.base.Function;
+
 public class AnnotationWrapper<T> {
-	protected Class<? extends Annotation> annotationClass;
+	protected final Class<? extends Annotation> annotationClass;
+	// should be BiFunction in JDK 8
+	protected final Function<T, String> function;
 
 	public AnnotationWrapper(Class<? extends Annotation> annotationClass) {
+		this(annotationClass, null);
+	}
+
+	public AnnotationWrapper(Class<? extends Annotation> annotationClass, Function<T, String> function) {
 		super();
 		this.annotationClass = annotationClass;
+		this.function = function;
 	}
 
 	public String getDisplayString(T t) {
+		if (function == null) {
+			return getAnnotationClassPart();
+		} else {
+			return String.format("%s%s", this.annotationClass, function.apply(t));
+		}
+	}
+
+	protected String getAnnotationClassPart() {
 		return String.format("@%s", annotationClass.getSimpleName());
 	}
 
 	public Class<? extends Annotation> getAnnotationClass() {
 		return annotationClass;
-	}
-
-	public void setAnnotationClass(Class<? extends Annotation> annotationClass) {
-		this.annotationClass = annotationClass;
 	}
 
 	@Override
